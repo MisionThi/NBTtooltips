9f26a7c94a4c0baae805dd27b1912b42f9d3404a
MisionThi <66966201+MisionThi@users.noreply.github.com>
2023-02-12 20:13:49 +0100

    Bugfix
    Changed default key to `left-alt`

--- src/main/java/net/mision_thi/nbttooltips/NBTtooltipsMod.java
+++ src/main/java/net/mision_thi/nbttooltips/NBTtooltipsMod.java
@@ -1,8 +1,9 @@
 package net.mision_thi.nbttooltips;
 
 import net.fabricmc.api.ModInitializer;
-import net.mision_thi.nbttooltips.config.ModConfigs;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.util.InputUtil;
+import net.mision_thi.nbttooltips.config.ModConfigs;
 import net.minecraft.client.option.KeyBinding;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
@@ -11,7 +12,7 @@ import org.slf4j.LoggerFactory;
 public class NBTtooltipsMod implements ModInitializer {
 	public static final String MOD_ID = "NBTtooltips";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final KeyBinding KEYBIND = KeyBindingHelper.registerKeyBinding(new KeyBinding("nbttooltips.keybind", GLFW.GLFW_KEY_LEFT_SHIFT, KeyBinding.INVENTORY_CATEGORY));
+	public static final KeyBinding KEYBIND = KeyBindingHelper.registerKeyBinding(new KeyBinding("nbttooltips.keybind", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT, KeyBinding.INVENTORY_CATEGORY));
 
 	@Override
 	public void onInitialize() {
